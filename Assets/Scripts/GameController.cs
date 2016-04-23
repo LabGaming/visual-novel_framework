253c3c5111425bf6625f96d4915499a4ff4d7408
Drasky-Vanderhoff <drasky.vanderhoff@gmail.com>
2016-04-23 02:06:09 -0300

    Initial Commit.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+	public Text gameContent;
+	private bool tooMuchWii;
+
+	// Use this for initialization
+	void Start () {
+		gameContent.text = "WIIIIII ANDA!!!";
+		tooMuchWii = false;
+	}
+
+	public void Wii () {
+		gameContent.text += "\n Wii!";
+	}
+
+	public void Quit () {
+		// TODO : DO IT RIGHT!!!
+		Application.Quit();
+	}
+}
