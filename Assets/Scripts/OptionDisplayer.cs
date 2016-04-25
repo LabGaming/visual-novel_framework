4a3ce2ed52f5eeeed076702aeebd7c42a5cd8989
Drasky-Vanderhoff <drasky.vanderhoff@gmail.com>
2016-04-25 01:51:53 -0300

    Dialog System - Options now work by clicking on the text of each one.

--- Assets/Scripts/OptionDisplayer.cs
+++ Assets/Scripts/OptionDisplayer.cs
@@ -3,4 +3,9 @@ using System.Collections;
 
 public class OptionDisplayer : MonoBehaviour {
 	public Option option;
+	public StoryEventsDisplayer displayer;
+
+	public void displayNextEvent() {
+		displayer.displayEvent(option.nextEvent);
+	}
 }
--- Assets/Scripts/StoryEventsDisplayer.cs
+++ Assets/Scripts/StoryEventsDisplayer.cs
@@ -14,21 +14,35 @@ public class StoryEventsDisplayer : MonoBehaviour {
 	private StoryEvent currentEvent;
 
 	void Start () {
-		this.displayEvent(initialEvent);
+		displayEvent(initialEvent);
 	}
 
-	void displayEvent (string eventName) {
+	#region Display Methods
+	public void displayEvent (string eventName) {
 		currentEvent = events.getEvent(eventName);
 		eventDisplayer.text = currentEvent.content;
 		displayOptionsForCurrentEvent();
 	}
 
-	void displayOptionsForCurrentEvent () {
+	private void displayOptionsForCurrentEvent () {
+		clearOptions();
 		foreach (var item in currentEvent.Options) {
 			GameObject g_go = GameObject.Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+
 			g_go.GetComponentInChildren<OptionDisplayer>().option = item;
+			g_go.GetComponentInChildren<OptionDisplayer>().displayer = this;
+			g_go.GetComponentInChildren<Button>().onClick.AddListener(
+				g_go.GetComponentInChildren<OptionDisplayer>().displayNextEvent
+			);
 			g_go.GetComponentInChildren<Text>().text = item.content;
 			g_go.transform.SetParent(optionsDisplayer.transform);
 		}
 	}
+
+	private void clearOptions () {
+		foreach (Transform child in optionsDisplayer.transform) {
+			GameObject.Destroy(child.gameObject);
+		}
+	}
+	#endregion
 }
