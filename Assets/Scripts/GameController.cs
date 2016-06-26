14115223028c81eb5d4b78466f6cfffb66618436
Drasky-Vanderhoff <drasky.vanderhoff@gmail.com>
2016-06-25 21:53:52 -0300

    Adding functionality for New Game and Exit buttons, not very exciting...

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,8 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
+
+	public void NewGame(string sceneName) {
+		SceneManager.LoadScene(sceneName);
+	}
 
 	public void Quit () {
 		// TODO : DO IT RIGHT!!!
